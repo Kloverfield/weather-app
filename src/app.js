7a19b0592cb73a5af41dbc450d2fc854d7250c49
Kloverfield <125980406+Kloverfield@users.noreply.github.com>
2023-06-19 14:27:27 +0200

    Added forecast-feature, removed 2 days of forecast and rearranged app.js
    HTML:
    Renamed the ID's for the forecast days and removed 2 days.
    
    JS:
    Applied changes in HTML.
    Fixed previous API-problem.
    Rearranged the search-functions;
     - 4 search interactions; display city, display date, display weekday names and display weather
     - 4 'display weather'-features, temperature, humidity, wind and 'feels like'-temperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 //METRIC CHANGE C/F
-
 function displayButtonUnit() {
   let buttonText = document.querySelector("#unit-button");
   if (buttonText.innerHTML === "°C") {
@@ -25,30 +24,24 @@ function changeTemp() {
   let buttonText = document.querySelector("#unit-button");
   let todayTemp = document.querySelector("#today-temp");
   let todayTempFeels = document.querySelector("#feelsLike");
-  let after1Temp = document.querySelector("#after1-temp");
-  let after2Temp = document.querySelector("#after2-temp");
-  let after3Temp = document.querySelector("#after3-temp");
-  let after4Temp = document.querySelector("#after4-temp");
-  let after5Temp = document.querySelector("#after5-temp");
-  let after6Temp = document.querySelector("#after6-temp");
+  let day2Temp = document.querySelector("#day2-temp");
+  let day3Temp = document.querySelector("#day3-temp");
+  let day4Temp = document.querySelector("#day4-temp");
+  let day5Temp = document.querySelector("#day5-temp");
   if (buttonText.innerHTML === "°C") {
     todayTemp.innerHTML = convertToFahrenheit(todayTemp.innerHTML);
     todayTempFeels.innerHTML = convertToFahrenheit(todayTempFeels.innerHTML);
-    after1Temp.innerHTML = convertToFahrenheit(after1Temp.innerHTML);
-    after2Temp.innerHTML = convertToFahrenheit(after2Temp.innerHTML);
-    after3Temp.innerHTML = convertToFahrenheit(after3Temp.innerHTML);
-    after4Temp.innerHTML = convertToFahrenheit(after4Temp.innerHTML);
-    after5Temp.innerHTML = convertToFahrenheit(after5Temp.innerHTML);
-    after6Temp.innerHTML = convertToFahrenheit(after6Temp.innerHTML);
+    day2Temp.innerHTML = convertToFahrenheit(day2Temp.innerHTML);
+    day3Temp.innerHTML = convertToFahrenheit(day3Temp.innerHTML);
+    day4Temp.innerHTML = convertToFahrenheit(day4Temp.innerHTML);
+    day5Temp.innerHTML = convertToFahrenheit(day5Temp.innerHTML);
   } else {
     todayTemp.innerHTML = convertToCelsius(todayTemp.innerHTML);
     todayTempFeels.innerHTML = convertToCelsius(todayTempFeels.innerHTML);
-    after1Temp.innerHTML = convertToCelsius(after1Temp.innerHTML);
-    after2Temp.innerHTML = convertToCelsius(after2Temp.innerHTML);
-    after3Temp.innerHTML = convertToCelsius(after3Temp.innerHTML);
-    after4Temp.innerHTML = convertToCelsius(after4Temp.innerHTML);
-    after5Temp.innerHTML = convertToCelsius(after5Temp.innerHTML);
-    after6Temp.innerHTML = convertToCelsius(after6Temp.innerHTML);
+    day2Temp.innerHTML = convertToCelsius(day2Temp.innerHTML);
+    day3Temp.innerHTML = convertToCelsius(day3Temp.innerHTML);
+    day4Temp.innerHTML = convertToCelsius(day4Temp.innerHTML);
+    day5Temp.innerHTML = convertToCelsius(day5Temp.innerHTML);
   }
 }
 
@@ -60,200 +53,13 @@ function changeMetric(event) {
 let unitButton = document.querySelector("#unit-button");
 unitButton.addEventListener("click", changeMetric);
 
-//SEARCH INTERACTION 8/8; DISPLAY TEMPERATURE OF EACH WEEKDAY
-
-function displayWeekdayTemp(response) {
-  console.log(response.data);
+//SEARCH INTERACTION 1/4; DISPLAY SEARCHED CITY
+function displayCity(cityString) {
+  let cityText = document.querySelector("#city");
+  cityText.innerHTML = cityString.trim().toUpperCase();
 }
 
-function getWeekTemp(response) {
-  let longitude = response.data.coord.lon;
-  let latitude = response.data.coord.lat;
-  let count = 1;
-  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
-  let queryPara = `lat=${latitude}&lon=${longitude}&cnt=${count}&appid=${apiKey}&units=metric`;
-  let dailyApiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?${queryPara}`;
-  axios.get(dailyApiUrl).then(displayWeekdayTemp);
-}
-
-function getWeekdayTemp(cityString) {
-  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
-  let queryPara = `q=${cityString}&appid=${apiKey}&units=metric`;
-  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?${queryPara}`;
-  axios.get(weatherApiUrl).then(getWeekTemp);
-}
-
-//SEARCH INTERACTION 7/8; DISPLAY WEEKDAYS
-
-function getDay2() {
-  let date = new Date();
-  let day = date.getDay();
-  if (day <= 5) {
-    return +1;
-  } else {
-    return -6;
-  }
-}
-function getDay3() {
-  let date = new Date();
-  let day = date.getDay();
-  if (day <= 4) {
-    return +2;
-  } else {
-    return -5;
-  }
-}
-function getDay4() {
-  let date = new Date();
-  let day = date.getDay();
-  if (day <= 3) {
-    return +3;
-  } else {
-    return -4;
-  }
-}
-function getDay5() {
-  let date = new Date();
-  let day = date.getDay();
-  if (day <= 2) {
-    return +4;
-  } else {
-    return -3;
-  }
-}
-function getDay6() {
-  let date = new Date();
-  let day = date.getDay();
-  if (day <= 1) {
-    return +5;
-  } else {
-    return -2;
-  }
-}
-function getDay7() {
-  let date = new Date();
-  let day = date.getDay();
-  if (day <= 0) {
-    return +6;
-  } else {
-    return -1;
-  }
-}
-
-function calculateDay() {
-  let day2 = getDay2();
-  let day3 = getDay3();
-  let day4 = getDay4();
-  let day5 = getDay5();
-  let day6 = getDay6();
-  let day7 = getDay7();
-  let dayNumber = [day2, day3, day4, day5, day6, day7];
-  return dayNumber;
-}
-
-function displayWeekday() {
-  let date = new Date();
-  let day2Text = document.querySelector("#day2");
-  let day3Text = document.querySelector("#day3");
-  let day4Text = document.querySelector("#day4");
-  let day5Text = document.querySelector("#day5");
-  let day6Text = document.querySelector("#day6");
-  let day7Text = document.querySelector("#day7");
-  let days = [
-    "SUNDAY",
-    "MONDAY",
-    "TUESDAY",
-    "WEDNESDAY",
-    "THURSDAY",
-    "FRIDAY",
-    "SATURDAY",
-  ];
-  let day = date.getDay();
-  let dayNumber = calculateDay();
-  day2Text.innerHTML = days[day + dayNumber[0]];
-  day3Text.innerHTML = days[day + dayNumber[1]];
-  day4Text.innerHTML = days[day + dayNumber[2]];
-  day5Text.innerHTML = days[day + dayNumber[3]];
-  day6Text.innerHTML = days[day + dayNumber[4]];
-  day7Text.innerHTML = days[day + dayNumber[5]];
-}
-
-//SEARCH INTERACTION 6/8; DISPLAY HUMIDITY OF TODAY
-
-function displayHumidity(response) {
-  let humidity = Math.round(response.data.main.humidity);
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `${humidity}%`;
-}
-
-function getHumidity(cityString) {
-  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
-  let queryPara = `q=${cityString}&appid=${apiKey}&units=metric`;
-  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?${queryPara}`;
-  axios.get(weatherApiUrl).then(displayHumidity);
-}
-
-//SEARCH INTERACTION 5/8; DISPLAY WINDSPEED OF TODAY
-
-function displayWind(response) {
-  let wind = Math.round(response.data.wind.speed);
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = `${wind}m/s`;
-}
-
-function getWind(cityString) {
-  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
-  let queryPara = `q=${cityString}&appid=${apiKey}&units=metric`;
-  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?${queryPara}`;
-  axios.get(weatherApiUrl).then(displayWind);
-}
-
-//SEARCH INTERACTION 4/8; DISPLAY 'TEMPERATURE FEELS LIKE' OF TODAY
-
-function displayFeels(response) {
-  let todayTempFeels = Math.round(response.data.main.feels_like);
-  let buttonText = document.querySelector("#unit-button");
-  let feelsLikeElement = document.querySelector("#feelsLike");
-
-  if (buttonText.innerHTML === "°C") {
-    let tempF = convertToFahrenheit(`${todayTempFeels}`);
-    feelsLikeElement.innerHTML = `${tempF}`;
-  } else {
-    feelsLikeElement.innerHTML = `${todayTempFeels}°C`;
-  }
-}
-
-function getFeels(cityString) {
-  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
-  let queryPara = `q=${cityString}&appid=${apiKey}&units=metric`;
-  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?${queryPara}`;
-  axios.get(weatherApiUrl).then(displayFeels);
-}
-
-//SEARCH INTERACTION 3/8; DISPLAY TEMPERATURE OF TODAY
-
-function displayTodayTemp(response) {
-  let todayTemp = Math.round(response.data.main.temp);
-  let tempElement = document.querySelector("#today-temp");
-  let buttonText = document.querySelector("#unit-button");
-
-  if (buttonText.innerHTML === "°C") {
-    let todayTempF = convertToFahrenheit(`${todayTemp}`);
-    tempElement.innerHTML = `${todayTempF}`;
-  } else {
-    tempElement.innerHTML = `${todayTemp}°C`;
-  }
-}
-
-function getTodayTemp(cityString) {
-  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
-  let queryPara = `q=${cityString}&appid=${apiKey}&units=metric`;
-  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?${queryPara}`;
-  axios.get(weatherApiUrl).then(displayTodayTemp);
-}
-
-//SEARCH INTERACTION 2/8; DISPLAY DATE OF TODAY
-
+//SEARCH INTERACTION 2/4; DISPLAY DATE OF TODAY
 function formatDate() {
   let currentDate = new Date();
   let days = [
@@ -290,29 +96,287 @@ function displayDate() {
   dateText.innerHTML = currentDate;
 }
 
-//SEARCH INTERACTION 1/8; DISPLAY SEARCHED CITY
+//SEARCH INTERACTION 3/4; DISPLAY WEEKDAY NAMES
+function getDay2() {
+  let date = new Date();
+  let day = date.getDay();
+  if (day <= 5) {
+    return +1;
+  } else {
+    return -6;
+  }
+}
+function getDay3() {
+  let date = new Date();
+  let day = date.getDay();
+  if (day <= 4) {
+    return +2;
+  } else {
+    return -5;
+  }
+}
+function getDay4() {
+  let date = new Date();
+  let day = date.getDay();
+  if (day <= 3) {
+    return +3;
+  } else {
+    return -4;
+  }
+}
+function getDay5() {
+  let date = new Date();
+  let day = date.getDay();
+  if (day <= 2) {
+    return +4;
+  } else {
+    return -3;
+  }
+}
 
-function displayCity(cityString) {
-  let cityText = document.querySelector("#city");
-  cityText.innerHTML = cityString.trim().toUpperCase();
+function calculateDay() {
+  let day2 = getDay2();
+  let day3 = getDay3();
+  let day4 = getDay4();
+  let day5 = getDay5();
+  let dayNumber = [day2, day3, day4, day5];
+  return dayNumber;
+}
+
+function displayWeekday() {
+  let date = new Date();
+  let day2Text = document.querySelector("#day2");
+  let day3Text = document.querySelector("#day3");
+  let day4Text = document.querySelector("#day4");
+  let day5Text = document.querySelector("#day5");
+  let days = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+  ];
+  let day = date.getDay();
+  let dayNumber = calculateDay();
+  day2Text.innerHTML = days[day + dayNumber[0]];
+  day3Text.innerHTML = days[day + dayNumber[1]];
+  day4Text.innerHTML = days[day + dayNumber[2]];
+  day5Text.innerHTML = days[day + dayNumber[3]];
+}
+
+//SEARCH INTERACTION 4/4; DISPLAY WEATHER INFORMATION
+
+//DISPLAY WEATHER INFORMATION 1/4; TEMPERATURE
+function displayTemp(weather) {
+  let day1Temp = Math.round(weather[0].main.temp);
+  let day2Temp = Math.round(weather[1].main.temp);
+  let day3Temp = Math.round(weather[2].main.temp);
+  let day4Temp = Math.round(weather[3].main.temp);
+  let day5Temp = Math.round(weather[4].main.temp);
+
+  let day1Element = document.querySelector("#today-temp");
+  let day2Element = document.querySelector("#day2-temp");
+  let day3Element = document.querySelector("#day3-temp");
+  let day4Element = document.querySelector("#day4-temp");
+  let day5Element = document.querySelector("#day5-temp");
+
+  let buttonText = document.querySelector("#unit-button");
+
+  if (buttonText.innerHTML === "°C") {
+    let temp1F = convertToFahrenheit(`${day1Temp}`);
+    day1Element.innerHTML = `${temp1F}`;
+    let temp2F = convertToFahrenheit(`${day2Temp}`);
+    day2Element.innerHTML = `${temp2F}`;
+    let temp3F = convertToFahrenheit(`${day3Temp}`);
+    day3Element.innerHTML = `${temp3F}`;
+    let temp4F = convertToFahrenheit(`${day4Temp}`);
+    day4Element.innerHTML = `${temp4F}`;
+    let temp5F = convertToFahrenheit(`${day5Temp}`);
+    day5Element.innerHTML = `${temp5F}`;
+  } else {
+    day1Element.innerHTML = `${day1Temp}°C`;
+    day2Element.innerHTML = `${day2Temp}°C`;
+    day3Element.innerHTML = `${day3Temp}°C`;
+    day4Element.innerHTML = `${day4Temp}°C`;
+    day5Element.innerHTML = `${day5Temp}°C`;
+  }
+}
+
+//DISPLAY WEATHER INFORMATION 2/4; HUMIDITY
+function displayHumidity(weather) {
+  let humidity = Math.round(weather.main.humidity);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${humidity}%`;
+}
+
+//DISPLAY WEATHER INFORMATION 3/4; WINDSPEED
+function displayWind(weather) {
+  let wind = Math.round(weather.wind.speed);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${wind}m/s`;
+}
+
+//DISPLAY WEATHER INFORMATION 4/4; TEMPERATURE 'FEELS LIKE'
+function displayFeels(weather) {
+  let todayTempFeels = Math.round(weather.main.feels_like);
+  let buttonText = document.querySelector("#unit-button");
+  let feelsLikeElement = document.querySelector("#feelsLike");
+
+  if (buttonText.innerHTML === "°C") {
+    let tempF = convertToFahrenheit(`${todayTempFeels}`);
+    feelsLikeElement.innerHTML = `${tempF}`;
+  } else {
+    feelsLikeElement.innerHTML = `${todayTempFeels}°C`;
+  }
+}
+
+function displayWeather(weather) {
+  // display weather information 1/4; temperature
+  displayTemp(weather);
+
+  // display weather information 2/4; 'feels like'-temperature
+  displayFeels(weather[0]);
+
+  // display weather information 3/4; windspeed
+  displayWind(weather[0]);
+
+  // display weather information 4/4; humidity
+  displayHumidity(weather[0]);
+}
+
+function generateDate() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  if (month <= 9 && day <= 9) {
+    return `${year}-0${month}-0${day}`;
+  } else {
+    if (month <= 9) {
+      return `${year}-0${month}-${day}`;
+    } else {
+      if (day <= 9) {
+        return `${year}-${month}-0${day}`;
+      } else {
+        return `${year}-${month}-${day}`;
+      }
+    }
+  }
+}
+
+function sortWeatherData(response) {
+  let bulk = response.data.list;
+  let apiDate = bulk[0].dt_txt;
+  let date = generateDate();
+  let day1 = bulk[0];
+
+  if (apiDate === `${date} 00:00:00`) {
+    // CHECK WHAT T0 DO WITH THIS. WHEN DOES THE DATE CHANGE FOR THE (API??? IF IT CHANGES BEFORE MIDNIGHT, WE HAVE A PROBLEM.
+    let day2 = bulk[1];
+    let day3 = bulk[1];
+    let day4 = bulk[1];
+    let day5 = bulk[1];
+    let days = [day1, day2, day3, day4, day5];
+    displayWeather(days);
+  } else {
+    if (apiDate === `${date} 03:00:00`) {
+      let day2 = bulk[11];
+      let day3 = bulk[19];
+      let day4 = bulk[27];
+      let day5 = bulk[35];
+      let days = [day1, day2, day3, day4, day5];
+      displayWeather(days);
+    } else {
+      if (apiDate === `${date} 06:00:00`) {
+        let day2 = bulk[10];
+        let day3 = bulk[18];
+        let day4 = bulk[26];
+        let day5 = bulk[31];
+        let days = [day1, day2, day3, day4, day5];
+        displayWeather(days);
+      } else {
+        if (apiDate === `${date} 09:00:00`) {
+          let day2 = bulk[9];
+          let day3 = bulk[17];
+          let day4 = bulk[25];
+          let day5 = bulk[30];
+          let days = [day1, day2, day3, day4, day5];
+          displayWeather(days);
+        } else {
+          if (apiDate === `${date} 12:00:00`) {
+            let day2 = bulk[8];
+            let day3 = bulk[16];
+            let day4 = bulk[24];
+            let day5 = bulk[32];
+            let days = [day1, day2, day3, day4, day5];
+            displayWeather(days);
+          } else {
+            if (apiDate === `${date} 15:00:00`) {
+              let day2 = bulk[7];
+              let day3 = bulk[15];
+              let day4 = bulk[23];
+              let day5 = bulk[31];
+              let days = [day1, day2, day3, day4, day5];
+              displayWeather(days);
+            } else {
+              if (apiDate === `${date} 18:00:00`) {
+                let day2 = bulk[6];
+                let day3 = bulk[14];
+                let day4 = bulk[22];
+                let day5 = bulk[30];
+                let days = [day1, day2, day3, day4, day5];
+                displayWeather(days);
+              } else {
+                let day2 = bulk[5];
+                let day3 = bulk[13];
+                let day4 = bulk[21];
+                let day5 = bulk[29];
+                let days = [day1, day2, day3, day4, day5];
+                displayWeather(days);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+function getWeather(response) {
+  let longitude = response.data.coord.lon;
+  let latitude = response.data.coord.lat;
+  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
+  let queryPara = `lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  let forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?${queryPara}`;
+  axios.get(forecastApiUrl).then(sortWeatherData);
+}
+
+function getCoord(cityString) {
+  let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
+  let queryPara = `q=${cityString}&appid=${apiKey}&units=metric`;
+  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?${queryPara}`;
+  axios.get(weatherApiUrl).then(getWeather);
 }
 
 //USE CURRENT LOCATION AS DEFAULT SEARCHINPUT
-
 function displayTempByLocation(response) {
   let city = response.data.name;
-  displayDate();
+  // search interaction 1/4: display searched city
   displayCity(city.trim().toLowerCase());
-  getTodayTemp(city.trim().toLowerCase());
-  getFeels(city.trim().toLowerCase());
-  getWind(city.trim().toLowerCase());
-  getHumidity(city.trim().toLowerCase());
 
+  // search interaction 2/4: display date of today
+  displayDate();
+
+  // search interaction 3/4: display weekday names
   displayWeekday();
-  getWeekdayTemp(city.trim().toLowerCase());
+
+  // search interaction 4/4: display weather information
+  getCoord(city.trim().toLowerCase());
 }
 
-function getTempByLocation(position) {
+function getDefaultWeather(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiKey = "6bd7c4bfa85bb276dc93ab103505c1de";
@@ -321,25 +385,27 @@ function getTempByLocation(position) {
   axios.get(weatherApiUrl).then(displayTempByLocation);
 }
 
-navigator.geolocation.getCurrentPosition(getTempByLocation);
+navigator.geolocation.getCurrentPosition(getDefaultWeather);
 
 //SEARCH-FUNCTION
-
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  if (searchInput.value === "") {
+  let city = searchInput.value;
+  if (city === "") {
     alert("Please type the name of a city 🙂");
   } else {
-    displayDate();
-    displayCity(searchInput.value.trim().toLowerCase());
-    getTodayTemp(searchInput.value.trim().toLowerCase());
-    getFeels(searchInput.value.trim().toLowerCase());
-    getWind(searchInput.value.trim().toLowerCase());
-    getHumidity(searchInput.value.trim().toLowerCase());
+    // search interaction 1/4: display searched city
+    displayCity(city.trim().toLowerCase());
 
+    // search interaction 2/4: display date of today
+    displayDate();
+
+    // search interaction 3/4: display weekday names
     displayWeekday();
-    getWeekdayTemp(searchInput.value.trim().toLowerCase());
+
+    // search interaction 4/4: display weather information
+    getCoord(city.trim().toLowerCase());
   }
 }
 
